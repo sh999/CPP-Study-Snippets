b396f7e48b41920dbbb052e85516b8a7531eeb9e
sh999 <satrio.h@gmail.com>
2015-03-24 06:25:21 -0400

    Modify gitignore for xcode and start Inheritance

---
+++ Inheritance/Inheritance/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Inheritance
+//
+//  Created by Satrio on 3/22/15.
+//  Copyright (c) 2015 Satrio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
