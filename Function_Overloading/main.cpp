f8d27d690fc1cec38642c153cb4cd99f1ad26288
sh999 <satrio.h@gmail.com>
2015-04-04 11:31:39 -0400

    Completed

--- Function_Overloading/main.cpp
+++ Function_Overloading/main.cpp
@@ -1,3 +1,23 @@
+/*
+Declare an int and a double
+Call a function with the same name that will
+print a unique message depending if the argument is
+an int or a double
+*/
+#include <iostream>
+using namespace std;
+
+void display(int a){
+	cout << "\nPrinting integer";
+}
+
+void display(double a){
+	cout << "\nPrinting double";
+}
+
 int main(){
-	
+	int i;
+	double d;
+	display(i);
+	display(d);
 }
