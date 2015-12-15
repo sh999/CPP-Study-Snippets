9ae98db04272d43e1aa8a8894430d9b831458b20
sh999 <satrio.h@gmail.com>
2015-12-14 23:20:21 -0500

    Add Set

---
+++ Set/Set/main.cpp
@@ -0,0 +1,19 @@
+/*
+ Study of Set implemented in standard library
+ */
+#include <iostream>
+#include <set>
+using namespace std;
+int main(){
+    // Create a set of integers
+    set<int> intSet;
+    // Put values in the set
+    intSet.insert(30000);
+    intSet.insert(234);
+    intSet.insert(333);
+    // Loop through the set and print each element
+    set<int>::iterator a;
+    for(a = intSet.begin(); a != intSet.end(); a++){
+        cout << *a << endl;;
+    }
+}
